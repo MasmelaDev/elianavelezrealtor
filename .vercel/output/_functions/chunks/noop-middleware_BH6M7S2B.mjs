22c777dbcd24e58d77db2aa14b287ec5c7e8d225
masmeladev <miguelmasmeladev@gmail.com>
2026-04-19 16:30:00 -0400

    Remove unwanted files and apply .gitignore

--- .vercel/output/_functions/chunks/noop-middleware_BH6M7S2B.mjs
+++
@@ -1,9 +0,0 @@
-import { am as NOOP_MIDDLEWARE_HEADER } from './astro/server_Bx_r-E3M.mjs';
-
-const NOOP_MIDDLEWARE_FN = async (_ctx, next) => {
-  const response = await next();
-  response.headers.set(NOOP_MIDDLEWARE_HEADER, "true");
-  return response;
-};
-
-export { NOOP_MIDDLEWARE_FN as N };
--- .vercel/output/functions/_render.func/dist/server/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_Beci30La.mjs';
-export { renderers } from '../renderers.mjs';
